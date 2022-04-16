a65077faf7e7b0317fa7791459a7f58d96df5723
Bruno Saturnino Alves <brunosaturnino55@gmail.com>
2022-04-16 10:03:13 -0300

    day9php

---
+++ exercicios/ex14.php
@@ -0,0 +1,9 @@
+<?php
+
+$saudacao = "Nobre senhor ";
+$nome = "João";
+$sobrenome = "da Silva";
+
+echo $saudacao . $nome . " " . $sobrenome . " voçê está na lista dos convidados.";
+
+?>
---
+++ exercicios/ex15.php
@@ -0,0 +1,20 @@
+<?php
+
+$v = "uva" == "uva";
+$f = "uva" == "maça";
+
+if($v) {
+    echo "\$v Verdadeido";
+} else {
+    echo "\$v Falso";
+}
+
+echo "<br>";
+
+if($f) {
+    echo "\$f Verdadeido";
+} else {
+    echo "\$f Falso";
+}
+
+?>
