6e46a2f3bea94502f70522dc9bcadd739dbcc326
Bruno Saturnino Alves <brunosaturnino55@gmail.com>
2022-04-19 19:47:22 -0300

    day10php

---
+++ exercicios/ex16.php
@@ -0,0 +1,22 @@
+<?php
+
+$num1 = 5;
+$num2 = 3;
+
+if($num1 == $num2) {
+    echo "num1 é igual num2 <br>";
+}
+
+if($num1 != $num2) {
+    echo "num1 é diferente de num2 <br>";
+}
+
+if($num1 === $num2) {
+    echo "num1 é identico num2 <br>";
+}
+
+if($num1 !== $num2) {
+    echo "num1  não é identico a num2 <br>";
+}
+
+?>
