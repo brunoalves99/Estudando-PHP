390469619b20c0dd334041be78df0e887d5b1cd9
Bruno Saturnino Alves <brunosaturnino55@gmail.com>
2022-04-22 16:54:12 -0300

    day12php

---
+++ exercicios/ex17.php
@@ -0,0 +1,15 @@
+<?php
+
+if(15 > 5 && 'João' === 'João') {
+    echo "if 1 vdd";
+}
+
+if('teste' > 5 && 1) {
+    echo "if 2 vdd";
+}
+
+if(2 == 3 && 5 >= 3) {
+    echo "if 3 vdd";
+}
+
+?>
---
+++ exercicios/ex18.php
@@ -0,0 +1,15 @@
+<?php
+
+if(12 < 5 || "João" === "João") {
+    echo "if 1 verdadeiro <br>";
+}
+
+if(1 < 5 || 1) {
+    echo "if 2 verdadeiro <br>";
+}
+
+if(20 === "20" || 51 >= 31) {
+    echo "if 3 verdadeiro <br>";
+}
+
+?>
